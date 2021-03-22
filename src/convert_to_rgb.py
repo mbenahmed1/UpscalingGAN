59f500620c6e21f510a7cfc7ec8b99f0ba4ce70e
pahoffmann <pahoffmann@uni-osnabrueck.de>
2021-03-22 15:12:10 +0100

    converting now via folder, added percentage stats

--- src/convert_to_rgb.py
+++ src/convert_to_rgb.py
@@ -3,11 +3,31 @@
 
 from PIL import Image
 import sys
+from os import walk
 
 if len(sys.argv) != 2:
-    print("Usage: $ python convert_to_rgb.py [0.jpg] [1.jpg] [2.jpg] ...")
+    print("Usage: $ python convert_to_rgb.py [folder_dest]")
 
-for arg in sys.argv[1:]:
-    image = Image.open(str(arg))
+_, _, filenames = next(walk(sys.argv[1]))
+
+num_files = len(filenames)
+print(str(num_files) + " files found in the given directory.")
+print("Starting conversion...")
+counter = 0
+first = True
+perc_count = 0
+split = int(num_files / 100)
+
+for img_path in filenames:
+
+    if counter == split or first:
+        print(str(perc_count) + "%")
+        perc_count += 1
+        counter = 0
+        first = False
+
+    full_path = sys.argv[1] + str(img_path)
+    image = Image.open(str(full_path))
     rgb = image.convert('RGB')
-    rgb.save(str(arg))
+    rgb.save(str(full_path))
+    counter += 1
