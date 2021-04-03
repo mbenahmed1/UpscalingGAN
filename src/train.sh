7123e12dbbfdaf62b5f286d86cfb99001b4957f3
Patrick Hoffmann <33060086+pahoffmann@users.noreply.github.com>
2021-04-03 19:23:11 +0200

    Revert "Sequential"

--- src/train.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for((i = 0; i < 100; i++))
-do
-	python train.py ../seq/gen ../seq/dis
-	mv ../seq/upscaled.pdf ../seq/${i}_up.pdf
-    mv ../seq/full_res_image.pdf ../seq/${i}_full.pdf
-    mv ../seq/low_res_image.pdf ../seq/${i}_low.pdf
-done    
